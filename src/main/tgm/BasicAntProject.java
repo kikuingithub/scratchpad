46a1064b2b5d36f8e49f30d9f7f1c61471c3dff9
Rene Hollander <rene@renesersatzlaptop>
2015-01-15 11:57:59 +0100

    deploy-BasicAntProject-1.0.0

--- src/main/tgm/BasicAntProject.java
+++ src/main/tgm/BasicAntProject.java
@@ -4,6 +4,7 @@ public class BasicAntProject {
   public static void main(String[] args) {
     BasicAntProject bap = new BasicAntProject();
     System.out.println(bap.getStringToPrint());
+    // test!
   }
 
   public String getStringToPrint() {
